3cdeee32017eabfc0d4ccf116f14874efa4e9639
dovla321 <dovla91mos@gmail.com>
2024-02-02 21:25:55 -1200

    aaa

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,4 @@
-import Recat from "react";
+import React from "react";
 
 function HomePage(){
     return(
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import Recat from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css"
